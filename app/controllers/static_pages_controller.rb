2c72a477b5e3960c8f0e5ff222d152e2da32b622
Quentin <berson.quentin@gmail.com>
2018-05-24 17:28:18 +0200

    debut twitch

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,5 @@
+require 'twitch-api'
+
 class StaticPagesController < ApplicationController
   def home
   end
@@ -6,5 +8,10 @@ class StaticPagesController < ApplicationController
   end
 
   def twitch
+    client = Twitch::Client.new client_id: "pvxwssoxxpdpl0qlzuzjezvqatz6jy"
+    @user = client.get_users({login: "freecodecamp"}).data
+
+    # @description = @user.description
+    @description = @user[0].description
   end
 end
